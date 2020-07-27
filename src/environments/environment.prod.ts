874a2c9511304a39e52352ac8fd5f5c50af84475
Rabi Róbert <ingenmaffen@gmail.com>
2020-07-27 14:37:23 +0200

    Update README and build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD3ZWLP5wSUKWTTYntOqI9swGgymY-Jm_A',
+    authDomain: 'fir-delivery-tracker.firebaseapp.com',
+    databaseURL: 'https://fir-delivery-tracker.firebaseio.com',
+    projectId: 'fir-delivery-tracker',
+    storageBucket: 'fir-delivery-tracker.appspot.com',
+    messagingSenderId: '66625706644',
+    appId: '1:66625706644:web:7e0a4653ff001e461b3281',
+    measurementId: 'G-TLLJDQ88FE',
+  },
 };
